7d8661d8b275819d4b6f91e968c19c5985e11bf6
matieo33 <61363778+matieo33@users.noreply.github.com>
2021-10-29 22:45:53 +0200

    Delete program.py

--- program.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-import requests, time
-process = 1
-while process == 1:
-    source = requests.get('https://www.robloxforum.com').text
-    soup = BeautifulSoup(source, 'lxml')
-    text = soup.find('span', class_='block-footer-counter').text
