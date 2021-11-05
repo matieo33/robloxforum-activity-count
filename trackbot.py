503d71bd27f42652cda87cf3c04d6a26f095b810
matieo33 <matieo33@wp.pl>
2021-11-06 00:49:41 +0100

    Update trackbot.py

--- trackbot.py
+++ trackbot.py
@@ -1,105 +1,108 @@
 try:
     from bs4 import BeautifulSoup
-    import requests, time, re
-    from colorama import Style
+    import requests
+    import time
+    import re
     from termcolor import colored
     from colorama import init
     import os
 except ModuleNotFoundError:
-    print("The program is missing essential libraries. Read the Github's tutorial how to install all the libraries.")
+    print(colored("The program is missing essential libraries. Read the Github's tutorial how to install all the libraries.", 'red'))
     os.exit(0)
 
 decision = ''
 init()
-print(Style.RESET_ALL)
 
 # DEFS BELOW
+
+
 def print_status():
-        obj = time.localtime()
-        currentime = time.asctime(obj)
-        if decision == 'SAMPLE' or decision.lower == 'sample':
-            pass
-        else:
-            time.sleep(a)
-        source = requests.get('https://www.robloxforum.com').text
-        soup = BeautifulSoup(source, 'lxml')
-        stringy = soup.find('span', class_='block-footer-counter').text
-        usernames = soup.find_all('span', class_=['username--style1', 'username--style2', 'username--style3', 'username--style4',
-                                                  'username--style5', 'username--style6', 'username--style7', 'username--style8', 'username--style9', 'username--style10'
-                                                  'username--style11'])
-        print(currentime)
-        print(stringy)
-        for span in usernames:
-            attr = span.attrs['class']
-            numbas = re.findall(r'\d+', str(attr))
-            if numbas[0] == "2":
-                print(span.text)
-            elif numbas[0] == "3":
-                print(colored(span.text, 'red', attrs=['bold']))
-            elif numbas[0] == "4":
-                print(colored(span.text, 'blue', attrs=['bold']))
-            elif numbas[0] == "6":
-                print(colored(span.text, 'green', attrs=['bold']))
-            elif numbas[0] == "7":
-                print(colored(span.text, 'green'))
-            elif numbas[0] == "8":
-                print(colored(span.text, 'blue'))
-            elif numbas[0] == "9":
-                print(colored(span.text, 'yellow'))
-            elif numbas[0] == "10":
-                def strike(text):
-                    return ''.join([u'\u0336{}'.format(c) for c in text])
-                black = (colored(span.text, 'yellow'))
-                print(strike(black))
-            elif numbas[0] == "11":
-                print(colored(span.text, 'blue', attrs=['bold']))
-        print('\n')
-        if decision == 'SAMPLE':
-                print("END OF SAMPLE")
-                print("Available options: TRACK SAMPLE HELP")
-        else:
-            if b == "Y":
-                with open("log.txt", "a") as o:
-                    encoded_string = stringy.encode("ascii", "ignore")
-                    decode_string = encoded_string.decode()
-                    o.write(decode_string)
-                    for span in usernames:
-                        attr = span.attrs['class']
-                        numbas = re.findall(r'\d+', str(attr))
-                        sp = span.text
-                        obj = time.localtime()
-                        currentime = time.asctime(obj)
-                        if c == "Y":
-                            o.write(currentime)
-                            if numbas[0] == "2":
-                                o.write(sp + " | normal user")
-                                o.write('\n')
-                            elif numbas[0] == "3":
-                                o.write(sp + " | administrator")
-                                o.write('\n')
-                            elif numbas[0] == "4":
-                                o.write(sp + " | moderator")
-                                o.write('\n')
-                            elif numbas[0] == "6":
-                                o.write(sp + " | verified")
-                                o.write('\n')
-                            elif numbas[0] == "7":
-                                o.write(sp + " | vip")
-                                o.write('\n')
-                            elif numbas[0] == "8":
-                                o.write(sp + " | pro")
-                                o.write('\n')
-                            elif numbas[0] == "9":
-                                o.write(sp + " | ultra")
-                                o.write('\n')
-                            elif numbas[0] == "10":
-                                o.write(sp + " | banned")
-                                o.write('\n')
+    obj = time.localtime()
+    currentime = time.asctime(obj)
+    if decision == 'SAMPLE' or decision.lower == 'sample':
+        pass
+    else:
+        time.sleep(a)
+    source = requests.get('https://www.robloxforum.com').text
+    soup = BeautifulSoup(source, 'lxml')
+    stringy = soup.find('span', class_='block-footer-counter').text
+    usernames = soup.find_all('span', class_=['username--style1', 'username--style2', 'username--style3', 'username--style4',
+                                              'username--style5', 'username--style6', 'username--style7', 'username--style8', 'username--style9', 'username--style10'
+                                              'username--style11'])
+    print(currentime)
+    print(stringy)
+    for span in usernames:
+        attr = span.attrs['class']
+        numbas = re.findall(r'\d+', str(attr))
+        if numbas[0] == "2":
+            print(span.text)
+        elif numbas[0] == "3":
+            print(colored(span.text, 'red', attrs=['bold']))
+        elif numbas[0] == "4":
+            print(colored(span.text, 'blue', attrs=['bold']))
+        elif numbas[0] == "6":
+            print(colored(span.text, 'green', attrs=['bold']))
+        elif numbas[0] == "7":
+            print(colored(span.text, 'green'))
+        elif numbas[0] == "8":
+            print(colored(span.text, 'blue'))
+        elif numbas[0] == "9":
+            print(colored(span.text, 'yellow'))
+        elif numbas[0] == "10":
+            def strike(text):
+                return ''.join([u'\u0336{}'.format(c) for c in text])
+            black = (colored(span.text, 'yellow'))
+            print(strike(black))
+        elif numbas[0] == "11":
+            print(colored(span.text, 'blue', attrs=['bold']))
+    print('\n')
+    if decision == 'SAMPLE':
+        print("END OF SAMPLE")
+        print("Available options: TRACK SAMPLE HELP")
+    else:
+        if b == "Y":
+            with open("log.txt", "a") as o:
+                encoded_string = stringy.encode("ascii", "ignore")
+                decode_string = encoded_string.decode()
+                o.write(decode_string)
+                if c == "Y": o.write(currentime + '\n')
+                for span in usernames:
+                    attr = span.attrs['class']
+                    numbas = re.findall(r'\d+', str(attr))
+                    sp = span.text
+                    obj = time.localtime()
+                    currentime = time.asctime(obj)
+                    if c == "Y":
+                        if numbas[0] == "2":
+                            o.write(sp + " | normal user")
                             o.write('\n')
-                        else:
-                            pass
-            else:
-                pass
+                        elif numbas[0] == "3":
+                            o.write(sp + " | administrator")
+                            o.write('\n')
+                        elif numbas[0] == "4":
+                            o.write(sp + " | moderator")
+                            o.write('\n')
+                        elif numbas[0] == "6":
+                            o.write(sp + " | verified")
+                            o.write('\n')
+                        elif numbas[0] == "7":
+                            o.write(sp + " | vip")
+                            o.write('\n')
+                        elif numbas[0] == "8":
+                            o.write(sp + " | pro")
+                            o.write('\n')
+                        elif numbas[0] == "9":
+                            o.write(sp + " | ultra")
+                            o.write('\n')
+                        elif numbas[0] == "10":
+                            o.write(sp + " | banned")
+                            o.write('\n')
+                        o.write('\n')
+                    else:
+                        pass
+        else:
+            pass
+
 
 def run():
     process = 1
@@ -108,10 +111,10 @@ def run():
 # DEFS ABOVE
 
 
-print("RF trackbot - credits to MATIEO33")
-print("RF: https://robloxforum.com/members/matieo33.8832/")
-print("Github: https://github.com/matieo33")
-print("Available options: TRACK SAMPLE HELP")
+print(colored("RF trackbot - credits to MATIEO33", 'blue'))
+print(colored("RF: https://robloxforum.com/members/matieo33.8832/", 'red'))
+print(colored("Github: https://github.com/matieo33", 'green'))
+print("Available options: TRACK SAMPLE HELP ")
 
 if __name__ == '__main__':
     in_menu = 1
@@ -121,8 +124,10 @@ if __name__ == '__main__':
             print("I made this bot purely for the purpose of entertainment, and if ever happens - maybe also will come in handy for somebody.")
             print("Wanna help this bot grow? DM me.")
             print('Important: CTRL + C will stop the program entirely! Make sure to answer with "Y" if you wish to save the data to a TXT file.')
-            print("TRACK: Prints the activity of the site per amount of seconds you select.")
-            print("SAMPLE: Prints the activity of the site one time as an example of the program's work.")
+            print(
+                "TRACK: Prints the activity of the site per amount of seconds you select.")
+            print(
+                "SAMPLE: Prints the activity of the site one time as an example of the program's work.")
         elif decision == "SAMPLE" or decision.lower() == 'sample':
             print('')
             print_status()
@@ -132,7 +137,13 @@ if __name__ == '__main__':
         else:
             print("You typed something that was never intended, retry.")
 
-    a = int(input("Every how much seconds do you wish to recieve updates on the site activity? "))
+    a = int(input(
+        "Every how much seconds do you wish to recieve updates on the site activity? "))
     b = str(input("Do you wish the data to be saved to a TXT file? (Y/N) "))
     if b == "Y":
         c = str(input('Do you wish to include the list of all online users? (Y/N) '))
+        while 1:
+            print_status()
+    else:
+        while 1:
+            print_status()
